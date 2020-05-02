d3d4b6656ce03c1a2d9090f8b7d2e39db24842fb
Stu Heiss <sheiss@highlandsolutions.com>
2020-05-02 13:10:12 -0500

    add type annotations, custom types, type aliases

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,10 +4,31 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array exposing (Array)
 
+type alias Photo = { url : String }
+type alias Model = { photos : List Photo, selectedUrl : String }
+type alias Msg = { data : String, description : String }
+
+photoArray : Array Photo
+photoArray = Array.fromList initialModel.photos
+
+urlPrefix : String
 urlPrefix =
   "http://elm-in-action.com/"
 
+initialModel : Model
+initialModel =
+  {
+    photos =
+      [ { url = "1.jpeg" }
+      , { url = "2.jpeg" }
+      , { url = "3.jpeg" }
+      ]
+  , selectedUrl = "1.jpeg"
+  }
+
+view : Model -> Html Msg
 view model =
   div [ class "content" ]
       [ h1 [] [ text "Photo Groove" ]
@@ -20,6 +41,7 @@ view model =
         []
       ]
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
       [ src (urlPrefix ++ thumb.url)
@@ -28,16 +50,7 @@ viewThumbnail selectedUrl thumb =
       ]
       []
 
-initialModel =
-  {
-    photos =
-      [ { url = "1.jpeg" }
-      , { url = "2.jpeg" }
-      , { url = "3.jpeg" }
-      ]
-  , selectedUrl = "1.jpeg"
-  }
-
+update : Msg -> Model -> Model
 update msg model =
   if msg.description == "ClickedPhoto" then
     { model | selectedUrl = msg.data }
