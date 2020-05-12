6b5d72647857fde6f45ab502048206c54adecc0b
Stu Heiss <sheiss@highlandsolutions.com>
2020-05-11 23:07:35 -0500

    Ports and subs, finish chapter 5
    Send to js over ports.
    Receive from js via subscriptions.
    Receive initial value from js via flags.
    Wrap js function in requestAnimationFrame to delay its execution until
    the next time Elm calls view and renders the result to the DOM.

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,13 +1,14 @@
-module PhotoGroove exposing (main)
+port module PhotoGroove exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (checked, class, classList, id, name, src, title, type_)
-import Html.Events exposing (onClick)
+import Html.Attributes as Attr exposing (checked, class, classList, id, name, src, title, type_)
+import Html.Events exposing (on, onClick)
 import Random
 import Http
-import Json.Decode exposing (Decoder, int, list, string, succeed)
+import Json.Decode exposing (Decoder, at, int, list, string, succeed)
 import Json.Decode.Pipeline exposing (optional, required)
+import Json.Encode as Encode
 
 type ThumbnailSize
   = Small
@@ -18,6 +19,15 @@ type Status
   = Loading
   | Loaded (List Photo) String
   | Errored String
+
+port setFilters : FilterOptions -> Cmd msg
+
+port activityChanges : (String -> msg) -> Sub msg
+
+type alias FilterOptions =
+  { url : String
+  , filters : List { name : String, amount : Float }
+  }
 
 type alias Photo =
   { url : String
@@ -37,8 +47,12 @@ buildPhoto url size title =
   { url = url, size = size, title = title }
 
 type alias Model =
-  {  status : Status
-  ,  chosenSize : ThumbnailSize
+  { status : Status
+  , activity : String
+  , chosenSize : ThumbnailSize
+  , hue : Int
+  , ripple : Int
+  , noise : Int
   }
 
 urlPrefix : String
@@ -48,13 +62,17 @@ urlPrefix =
 initialModel : Model
 initialModel =
   { status = Loading
+  , activity = ""
   , chosenSize = Medium
+  , hue = 5
+  , ripple = 5
+  , noise = 5
   }
 
 initialCmd : Cmd Msg
 initialCmd =
   Http.get
-    { url = "http://elm-in-action.com/photos/list.json"
+    { url = urlPrefix ++ "photos/list.json"
     , expect = Http.expectJson GotPhotos (list photoDecoder)
     }
 
@@ -63,39 +81,55 @@ view model =
   div [ class "content" ] <|
     case model.status of
       Loaded photos selectedUrl ->
-        viewLoaded photos selectedUrl model.chosenSize
+        viewLoaded photos selectedUrl model
       Loading ->
         [ text "Loading..." ]
       Errored errorMessage ->
         [ text ("Error: " ++ errorMessage)]
 
-viewLoaded : List Photo -> String -> ThumbnailSize -> List (Html Msg)
-viewLoaded photos selectedUrl chosenSize =
-      [ h1 [] [ text "Photo Groove" ]
-      , button
-        [ onClick ClickedSurpriseMe ]
-        [ text "Surprise Me!" ]
-      , h3 [] [ text "Thumbnail size:" ]
-      , div [ id "choose-size" ]
-        (List.map (viewSizeChooser chosenSize) [ Small, Medium, Large ])
-      , div [ id "thumbnails", class (sizeToClass chosenSize) ]
-        (List.map (viewThumbnail selectedUrl) photos)
-      , img
-        [ class "large"
-        , src (urlPrefix ++ "large/" ++ selectedUrl)
-        ]
-        []
+viewFilter : (Int -> Msg) -> String -> Int -> Html Msg
+viewFilter toMsg name magnitude =
+  div [ class "filter-slider" ]
+    [ label [] [ text name ]
+    , rangeSlider
+      [ Attr.max "11"
+      , Attr.property "val" (Encode.int magnitude)
+      , onSlide toMsg
       ]
+      []
+    , label [] [ text (String.fromInt magnitude) ]
+    ]
+
+viewLoaded : List Photo -> String -> Model -> List (Html Msg)
+viewLoaded photos selectedUrl model =
+  [ h1 [] [ text "Photo Groove" ]
+  , button
+    [ onClick ClickedSurpriseMe ]
+    [ text "Surprise Me!" ]
+  , div [ class "activity" ] [ text model.activity ]
+  , div [ class "filters" ]
+    [ viewFilter SlidHue "Hue" model.hue
+    , viewFilter SlidRipple "Ripple" model.ripple
+    , viewFilter SlidNoise "Noise" model.noise
+    ]
+  , h3 [] [ text "Thumbnail size:" ]
+  , div [ id "choose-size" ]
+    (List.map (viewSizeChooser model.chosenSize) [ Small, Medium, Large ])
+  , div [ id "thumbnails", class (sizeToClass model.chosenSize) ]
+    (List.map (viewThumbnail selectedUrl) photos)
+  , canvas [ id "main-canvas", class "large" ] []
+  -- , img [ class "large", src (urlPrefix ++ "large/" ++ selectedUrl) ] []
+  ]
 
 viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
-    img
-      [ src (urlPrefix ++ thumb.url)
-      , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ " KB]")
-      , classList [ ( "selected", selectedUrl == thumb.url ) ]
-      , onClick (ClickedPhoto thumb.url)
-      ]
-      []
+  img
+    [ src (urlPrefix ++ thumb.url)
+    , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ " KB]")
+    , classList [ ( "selected", selectedUrl == thumb.url ) ]
+    , onClick (ClickedPhoto thumb.url)
+    ]
+    []
 
 sizeToClass : ThumbnailSize -> String
 sizeToClass size = sizeToString size
@@ -122,23 +156,29 @@ type Msg
   | ClickedSize ThumbnailSize
   | ClickedSurpriseMe
   | GotRandomPhoto Photo
+  | GotActivity String
   | GotPhotos (Result Http.Error (List Photo))
+  | SlidHue Int
+  | SlidRipple Int
+  | SlidNoise Int
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
+    GotActivity activity ->
+      ( { model | activity = activity }, Cmd.none )
     GotPhotos (Ok photos) ->
       case photos of
         first :: rest ->
-          ( { model | status = Loaded photos first.url }, Cmd.none )
+          applyFilters { model | status = Loaded photos first.url }
         [] ->
           ( { model | status = Errored "0 photos found" }, Cmd.none )
     GotPhotos (Err _) ->
       ( { model | status = Errored "Server error!" }, Cmd.none )
     GotRandomPhoto photo ->
-      ( { model | status = selectUrl photo.url model.status }, Cmd.none )
+      applyFilters { model | status = selectUrl photo.url model.status }
     ClickedPhoto url ->
-      ( { model | status = selectUrl url model.status }, Cmd.none )
+      applyFilters { model | status = selectUrl url model.status }
     ClickedSurpriseMe ->
       case model.status of
         Loaded (firstPhoto::otherPhotos) _ ->
@@ -153,6 +193,31 @@ update msg model =
           ( model, Cmd.none )
     ClickedSize size ->
       ( { model | chosenSize = size }, Cmd.none )
+    SlidHue hue ->
+      applyFilters { model | hue = hue }
+    SlidRipple ripple ->
+      applyFilters { model | ripple = ripple }
+    SlidNoise noise ->
+      applyFilters { model | noise = noise }
+
+applyFilters : Model -> ( Model, Cmd Msg )
+applyFilters model =
+  case model.status of
+    Loaded photos selectedUrl ->
+      let
+        filters =
+          [ { name = "Hue", amount = toFloat model.hue / 11 }
+          , { name = "Ripple", amount = toFloat model.ripple / 11 }
+          , { name = "Noise", amount = toFloat model.noise / 11 }
+          ]
+        url =
+          urlPrefix ++ "large/" ++ selectedUrl
+      in
+      ( model, setFilters { url = url, filters = filters } )
+    Loading ->
+      ( model, Cmd.none )
+    Errored errorMessage ->
+      ( model, Cmd.none )
 
 selectUrl : String -> Status -> Status
 selectUrl url status =
@@ -164,11 +229,32 @@ selectUrl url status =
     Errored _ ->
       status
 
-main : Program () Model Msg
+main : Program Float Model Msg
 main =
   Browser.element
-    { init = \_ -> ( initialModel, initialCmd )
+    { init = init
     , view = view
     , update = update
-    , subscriptions = \_ -> Sub.none
+    , subscriptions = subscriptions
     }
+
+init : Float -> ( Model, Cmd Msg )
+init flags =
+  let
+    activity = "Initializing Pasta v" ++ String.fromFloat flags
+  in
+    ( { initialModel | activity = activity }, initialCmd )
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  activityChanges GotActivity
+
+rangeSlider : List (Attribute msg) -> List (Html msg) -> Html msg
+rangeSlider attributes children =
+  node "range-slider" attributes children
+
+onSlide : (Int -> msg) -> Attribute msg
+onSlide toMsg =
+  at [ "detail", "userSlidTo" ] int
+    |> Json.Decode.map toMsg
+    |> on "slide"
